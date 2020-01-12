c41fab905fc1419eededf4d8c80fe6e841813156
Vano <myEmail@example.com>
2020-01-12 20:42:44 +0300

    add 1

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ class Hello
 	public static void main(String[] args)
 		{ 
 			System.out.println("A"); 
-			System.out.println("Hello World!1"); 
+			System.out.println("Hello World!3"); 
 			System.out.println("B");
 		} 
 }
